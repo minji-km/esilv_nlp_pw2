4d2fef7c7b669003a4e27c525f847ace2888f6fd
minji-km <115076549+minji-km@users.noreply.github.com>
2023-11-09 20:14:48 +0100

    debug

--- pages/2_pos_tagging.py
+++ pages/2_pos_tagging.py
@@ -35,6 +35,7 @@ def highlight_tagged_words(tagged_words):
     html = ""
     for word, pos_tag in tagged_words:
         background_color = pos_background_colors.get(pos_tag, 'transparent')  # Use full POS tag
+        print(f"Word: {word}, POS Tag: {pos_tag}, Background Color: {background_color}")
         html += f'<span style="background-color:{background_color}; padding: 2px;">{word}</span> '
 
     return html
