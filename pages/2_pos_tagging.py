02ec44af7f813d6eb647baa43693c2fc6866755c
minji-km <115076549+minji-km@users.noreply.github.com>
2023-11-09 19:48:55 +0100

    css

--- pages/2_pos_tagging.py
+++ pages/2_pos_tagging.py
@@ -20,12 +20,34 @@ def pos_tagging(sentence):
 
     return tagged_words
 
+def highlight_tagged_words(tagged_words):
+    # Define colors for different parts of speech
+    pos_colors = {
+        'NN': 'red',
+        'VB': 'blue',
+        'JJ': 'green',
+        # Add more POS tags and colors as needed
+    }
+
+    # Create HTML with inline styles for highlighting
+    html = ""
+    for word, pos_tag in tagged_words:
+        color = pos_colors.get(pos_tag[:2], 'black')  # Default to black if not defined
+        html += f'<span style="color:{color};">{word}</span> '
+
+    return html
+
 st.title('Part-of-Speech Tagging for Sentiment Analysis')
 st.write('Part-of-Speech tagging')
 
 # Input text box
-sentence = st.text_area("Enter a sentence :")
+sentence = st.text_area("Enter a sentence:")
 if sentence:
     # Perform part-of-speech tagging
     tagged_words = pos_tagging(sentence)
 
+    # Highlight words with colors
+    highlighted_html = highlight_tagged_words(tagged_words)
+
+    # Display the result with highlighted words
+    st.markdown(highlighted_html, unsafe_allow_html=True)
