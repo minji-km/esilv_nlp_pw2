6582c8b14f0d0e20932525838430f6b188f2a697
minji-km <minji.kmk@gmail.com>
2023-11-14 18:33:43 +0100

    test legend color

--- pages/2_pos_tagging.py
+++ pages/2_pos_tagging.py
@@ -1,14 +1,15 @@
-import streamlit as st
 import nltk
+import streamlit as st
 
 nltk.download('punkt')
-from nltk.tag import UnigramTagger
+nltk.download('treebank')
 
 # Load the tagged sentences for training
-train_sents = nltk.corpus.treebank.tagged_sents()[:3000]
+treebank_corpus = nltk.corpus.treebank.tagged_sents()
+train_sents = treebank_corpus[:3000]
 
 # Train the UnigramTagger
-tagger = UnigramTagger(train_sents)
+tagger = nltk.tag.UnigramTagger(train_sents)
 
 def pos_tagging(sentence):
     # Tokenize the input sentence
@@ -28,34 +29,27 @@ def highlight_tagged_words(tagged_words):
         'PR': 'lightgray',
     }
 
+    legend_html = "Legend: "
     highlighted_html = ""
     for word, pos_tag in tagged_words:
         background_color = pos_background_colors.get(pos_tag[:2], 'transparent') if pos_tag else 'transparent'
-        highlighted_html += f'<span style="background-color: {background_color};">{pos_tag}</span> '
+        highlighted_html += f'<span style="background-color: {background_color};">{word}</span> '
+        
+    for pos_tag, color in pos_background_colors.items():
+        legend_html += f'<span style="background-color: {color}; margin-right: 10px;">{pos_tag}</span> '
 
-    return highlighted_html
+    return legend_html, highlighted_html
 
 st.title('Part-of-Speech Tagging for Sentiment Analysis')
 st.write('Part-of-Speech tagging')
 
 # Input text box
 sentence = st.text_area("Enter a sentence:")
-
-# Button to trigger tagging
-if st.button("Tag and Highlight"):
+if sentence:
     # Perform part-of-speech tagging
     tagged_words = pos_tagging(sentence)
 
-    # Highlight words with background colors
-    highlighted_html = highlight_tagged_words(tagged_words)
-
-    # Display the result with highlighted words and background colors
+    # Highlight words with background colors and display legend
+    legend_html, highlighted_html = highlight_tagged_words(tagged_words)
+    st.markdown(legend_html, unsafe_allow_html=True)
     st.markdown(highlighted_html, unsafe_allow_html=True)
-
-    # Display the simplified legend
-    st.markdown("Legend:")
-    st.markdown("- Nouns", unsafe_allow_html=True)
-    st.markdown("- Verbs", unsafe_allow_html=True)
-    st.markdown("- Adjectives", unsafe_allow_html=True)
-    st.markdown("- Pronouns", unsafe_allow_html=True)
-    st.markdown("- Adverbs", unsafe_allow_html=True)
